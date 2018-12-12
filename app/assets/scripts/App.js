9853309af2919f3edfcba9fb930a96d49706c44c
Morly <morlysumibcay@gmail.com>
2018-12-12 23:50:11 +0800

    Completed Reveal on Scroll Behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
-var $ = require('jquery');
-
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "90%");
